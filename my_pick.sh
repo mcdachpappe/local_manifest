2d298e5df18fd013027235d5eeea42014e02a5f2
mcdachpappe <kackwiese@web.de>
2017-04-22 15:22:37 +0200

    Update my_pick.sh

--- my_pick.sh
+++ my_pick.sh
@@ -9,11 +9,21 @@ popd
 
 #add Magisk
 pushd build
-wget https://github.com/nvertigo/android_build/commit/4bd296c062c8d17ec6e443c5fed46dc3c1813654.patch -O - | git am
+#Build: Switch to Magisk as the built in root
+wget https://github.com/nvertigo/android_build/commit/63a305e3312595f73d4e037e66bdfed87ecc1d20.patch -O - | git am
+#magisk: delete magisk.zip from system after flashing
+wget https://github.com/nvertigo/android_build/commit/ab9e918cfabb27f9895d1738af2cf226708361a5.patch -O - | git am
 popd
 
 #microG
 pushd frameworks/base
-wget https://github.com/nvertigo/android_frameworks_base/commit/5a48dd6bfec4bc3179e983e8fdd088c3bedeedfd.patch -O - | git am
-wget https://github.com/nvertigo/android_frameworks_base/commit/d3152578850575e64c12d85ac42b6d7a1e5d827d.patch -O - | git am
+#Add signature Spoofing permission
+wget https://github.com/nvertigo/android_frameworks_base/commit/ab2aa7fbb522e8b4a85772836345382d22e4ee13.patch -O - | git am
+#Allow location providers also outside of /system
+wget https://github.com/nvertigo/android_frameworks_base/commit/6408344bc9ef06dd264a5ad4431659e21ff675d5.patch -O - | git am
 popd
+
+#Nightdisplay - MERGED
+#pushd device/oneplus/oneplus3t
+#git fetch git fetch https://review.lineageos.org/LineageOS/android_device_oneplus_oneplus3 refs/changes/68/165968/1 && git cherry-pick FETCH_HEAD
+#popd
