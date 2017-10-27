974bab3722cfb0653f11538bb23bcb6b2c5ff716
mcdachpappe <kackwiese@web.de>
2017-10-27 20:17:18 +0200

    Update my_reset.sh

--- my_reset.sh
+++ my_reset.sh
@@ -5,12 +5,12 @@ RESTORE="\033[0m"
 
 rm -r ./out
 
-repo sync -j$( nproc --all ) --force-sync -c --no-clone-bundle --no-tags --optimized-fetch --prune
+repo sync -j4 --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 repo forall -c git reset --hard
 repo forall -c git clean -f
 
-repo sync -j$( nproc --all ) --prune -d
+repo sync -j4 --prune -d
 
 repo status
 
