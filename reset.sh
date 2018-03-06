e7690ee5ecc8b74892f43755fcbf01ab3c6042e5
mcdachpappe <kackwiese@web.de>
2018-03-06 12:37:47 +0100

    Update and rename my_reset.sh to reset.sh

--- reset.sh
+++ reset.sh
@@ -5,7 +5,7 @@ RESTORE="\033[0m"
 
 rm -r ./out
 
-repo sync -j4 --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+repo sync -j6 --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 repo forall -c git reset --hard
 repo forall -c git clean -f
