96e2b2adb8de8328fa3c10f3d34d102a2c0480e2
mcdachpappe <kackwiese@web.de>
2018-03-08 16:05:54 +0100

    Update buildrom.sh

--- buildrom.sh
+++ buildrom.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# if you want to build without using ccache, comment the next 4 lines
+# setting up ccache to speed up building time
 export USE_CCACHE=1
-export CCACHE_DIR=/mnt/hgfs/Android/.ccache
-export CCACHE_MAX_SIZE=50G
+export CCACHE_DIR=/home/$USER/.ccache
+export CCACHE_MAX_SIZE=20G
 ccache -M $CCACHE_MAX_SIZE
 
 # encapsulate the build's temp directory.
@@ -14,7 +14,7 @@ TEMP=$TMP
 
 export TMP TMPDIR TEMP
 
-#make sure jack-server is restarted in TMP
+# make sure jack-server is restarted in TMP
 prebuilts/sdk/tools/jack-admin kill-server
 
 # we want all compiler messages in English
@@ -23,14 +23,11 @@ export LANGUAGE=C
 # set up the environment (variables and functions)
 source build/envsetup.sh
 
-# clean the out dir; comment out, if you want to do
-# a dirty build
+# clean the out dir; comment out, if you want to do a dirty build
 make -j9 ARCH=arm clean
 
-# fire up the building process and also log stdout
-# and stderrout
-breakfast oneplus3t 2>&1 | tee breakfast.log && \
-brunch oneplus3t 2>&1 | tee make.log
+# fire up the building process and also log stdout and stderrout
+brunch oneplus3 2>&1 | tee make.log
 
 # remove all temp directories
 rm -r ${TMP}
