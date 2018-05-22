b2b27a4d44a7f506546f4a1cb03cc07bdf71df4e
danny <noreference@web.de>
2018-05-22 12:57:22 +0200

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
+
 
 RED="\033[01;31m"
 RESTORE="\033[0m"
 
 make clean
 
-repo sync -j6 -f --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+repo sync -j12 -f --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 repo forall -c git reset --hard
 repo forall -c git clean -f
 
-repo sync -j6 --prune -d
+repo sync -j12 --prune -d
 
 repo status
 
 echo -e ${RED}"********"${RESTORE}
 echo -e ${RED}"* done *"${RESTORE}
 echo -e ${RED}"********"${RESTORE}
-
